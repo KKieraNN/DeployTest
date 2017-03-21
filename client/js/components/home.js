dc9c730ca04d1920aa036a2753115d7dca877580
Nick <nick@itprojmgt.com>
2017-03-21 11:11:44 -0600

    initial setup

--- client/js/components/home.js
+++ client/js/components/home.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react'
 
 import Header from './head/header'
+import MeetupList from './meetup/meetupList'
 
-
-export default class BusinessIndex extends Component {
+class Home extends Component {
 
   render() {
     return (
       <div>
         <Header />
+        <MeetupList />
       </div>
     )
   }
 }
+
+export default Home
--- client/js/reducers/index.js
+++ client/js/reducers/index.js
@@ -1,9 +1,22 @@
 import { combineReducers } from 'redux'
-// import your reducer files
-// import YourReducer from './file.js'
-// then include in CombineReducers object
-// yourreducer: YourReducer
 
-export default CombineReducers({
+import MeetupReducer from './MeetupReducer'
+// import AuthReducer from './AuthReducer'
+// import ChatReducer from './ChatReducer'
+// import FilterReducer from './FilterReducer'
+// import MeetupFormReducer from './MeetupFormReducer'
+// import RsvpReducer from './RsvpReducer'
+// import UserReducer from './UserReducer'
 
-});
+const rootReducer = combineReducers({
+  meetups: MeetupReducer,
+})
+
+// auth: AuthReducer,
+// chat: ChatReducer,
+// user: UserReducer,
+// filter: FilterReducer,
+// meetupForm: MeetupFormReducer,
+// rsvp: RsvpReducer
+
+export default rootReducer
